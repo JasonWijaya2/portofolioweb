6cf62c00e49a81c4d0cefe3d4d467fe08d364159
JasonWijaya2 <jasonwijaya2@gmail.com>
2024-02-27 18:49:08 +0700

    vcyber project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,4 @@
-import { newsWebsite, portofolioImg } from '@/public'
+import { newsWebsite, portofolioImg, vcyber } from '@/public'
 import SectionTitle from './SectionTitle'
 import Image from 'next/image'
 import { TbBrandGithub } from 'react-icons/tb'
@@ -115,6 +115,53 @@ const Projects = () => {
             </div>
         </div>
         {/* Project Three */}
+        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
+            <div className='flex flex-col xl:flex-row gap-6'>
+                <a
+                  className='w-full xl:w-1/2 h-auto relative group'
+                  href=""
+                  target='_blank'>
+                <div>
+                    <Image className="w-full h-full object-contain"
+                      src={vcyber}
+                      alt='vcyberImg'/>
+                </div>
+                </a>
+                <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10'>
+                    <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                        Feature Project
+                    </p>
+                    <h3 className='text-2xl font-bold'>
+                        V Cyber Gaming Gear Shop Website
+                    </h3>
+                    <p className='bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md'>
+                        This is a gaming gear sales simulation website that implements Redux state management. This website can sign in to a google account using Next Auth and can simulate payments using Stripe.com. While this website is still not responsive for the mobile version.
+                    </p>
+                    <ul className='text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark'>
+                        <li>Nextjs</li>
+                        <li>Typescript</li>
+                        <li>Tailwindcss</li>
+                        <li>StripeJS</li>
+                        <li>NextAuth</li>
+                        <li>Redux</li>
+                    </ul>
+                    <div className='text-2xl flex gap-4'>
+                        <a
+                          className='hover:text-textGreen duration-300'
+                          href="https://github.com/JasonWijaya2/vcyber-shop"
+                          target='_blank'>
+                            <TbBrandGithub />
+                        </a>
+                        <a
+                          className='hover:text-textGreen duration-300'
+                          href="https://vcyber-shop.vercel.app/"
+                          target='_blank'>
+                            <RxOpenInNewWindow />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
--- public/index.tsx
+++ public/index.tsx
@@ -4,5 +4,6 @@ import raionImg from "./assets/Raion-Sertificate.jpg"
 import portofolioImg from "./assets/portofolioImg.jpg"
 import newsWebsite from "./assets/news-website.jpg"
 import logo from "./assets/logo.png"
+import vcyber from "./assets/vcyber.jpg"
 
-export { profileImg, ruangguruImg, raionImg, portofolioImg, newsWebsite, logo }
+export { profileImg, ruangguruImg, raionImg, portofolioImg, newsWebsite, vcyber, logo }
